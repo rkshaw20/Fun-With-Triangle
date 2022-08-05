5225017918c08defc49d078a09585e6e8916b329
Raj Kishor Shaw <rkshaw2026@gmail.com>
2022-08-05 19:04:55 +0530

    feat: added qus in quiz

--- Quiz/quiz.js
+++ Quiz/quiz.js
@@ -2,7 +2,7 @@ const quizform= document.querySelector('.quiz-form');
 const submit=document.querySelector('.btn');
 const message=document.querySelector('#output-msg');
 
-const correctAns= ['90°', 'right angled']
+const correctAns= ['90°', 'right angled', '3 cm^2', '180°', '12 cm'];
 
 submit.addEventListener('click', calculatescore);
 
