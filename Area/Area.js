1cc36e03eb530b1d2eaf7811380e766602f8c49a
Raj Kishor Shaw <rkshaw2026@gmail.com>
2022-08-15 17:34:31 +0530

    feat:added negative validation check

--- Area/Area.js
+++ Area/Area.js
@@ -6,12 +6,20 @@ const message =document.querySelector('#output-msg');
 button.addEventListener('click', calculateArea);
 
 function calculateArea(){
-   if(base.value && height.value){
-    //const area = (base.value * height.value) / 2;
+   if(base.value>0 && height.value>0){
       const area=(Number(base.value)*Number(height.value))/2;
      message.innerText=`The area of the triangle is ${area.toFixed(2)} cm².`
 
-   }else{
+   }else if(base.value<0 && height.value<0){
+    message.innerText="The base and height cannot be negative."
+   }
+   else if(base.value<0){
+    message.innerText="The base cannot be negative."
+   }
+   else if(height.value<0){
+    message.innerText="The height cannot be negative."
+   }
+   else{
     message.innerText="Please enter value in all the fields."
    }
 }
